a6ce0cc4175c0e987d9f235115e6f787272428f3
codingninja2 <edwinafatsawo@gmail.com>
2024-12-06 20:00:33 +0000

    first Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#creating different variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+# user input
+name = input("enter your name:")
+
+print("\n Hello", name, "\n Welcome to Codingal")
